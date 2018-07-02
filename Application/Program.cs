cf6e7ecee3e90857ca2a00d083637b191cc74922
Domenic Helfenstein <dom.helfenstein@gmail.com>
2018-07-02 22:08:59 +0200

    Animation dargestellt

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,12 +1,29 @@
 ﻿namespace Application
 {
     using System;
+    using System.Threading;
 
     public static class Program
     {
         public static void Main(string[] _)
         {
-            Console.WriteLine("Hello World!");
+            var input =
+@"
+...................
+......oo......o...o
+.....oooo......ooo.
+oo....ooooo...oo..o
+ooo...oo..o...oooo.
+...................";
+
+            var gameOfLife = new GameOfLife();
+            while (true)
+            {
+                Console.Clear();
+                Console.Write(input);
+                input = gameOfLife.NextStep(input);
+                Thread.Sleep(500);
+            }
         }
     }
 }
